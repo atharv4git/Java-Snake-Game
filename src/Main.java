856182b937573c51863664f1e56e6635e15dedc0
atharv4git <atharv4study@gmail.com>
2023-04-07 12:27:30 +0530

    TODO added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    // TODO: make an array of 32x32 bits of 0s and 1s and display them in a grid fashion on a visual canvas.
     public static void main(String[] args) {
         System.out.println("hello");
     }
